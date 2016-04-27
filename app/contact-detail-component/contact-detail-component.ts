49b99c161df64288b7a829f012c15abd4d2203b2
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-04-27 21:21:03 +0200

    step 4: upgrade zippy-component and use from ng2

--- app/contact-detail-component/contact-detail-component.ts
+++ app/contact-detail-component/contact-detail-component.ts
@@ -1,10 +1,23 @@
 import {Component, Input, Inject} from '@angular/core';
 import {Contact} from '../models/contact';
+import {upgradeAdapter} from '../upgrade-adapter';
+
+const ZippyComponent = upgradeAdapter.upgradeNg1Component('zippyComponent');
 
 @Component({
   selector: 'contact-detail-component',
-  templateUrl: 'app/contact-detail-component/contact-detail-component.html'
+  templateUrl: 'app/contact-detail-component/contact-detail-component.html',
+  directives: [ZippyComponent]
 })
 export class ContactDetailComponent {
   @Input() contact: Contact;
+  zippyCaption: string;
+
+  constructor () {
+    this.toggleCaption(false);
+  }
+
+  toggleCaption (closed: boolean) {
+    this.zippyCaption = closed ? 'Show address' : 'Hide address';
+  }
 }
